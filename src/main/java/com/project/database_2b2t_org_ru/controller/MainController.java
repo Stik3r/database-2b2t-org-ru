11d52068960f7d7c81aa0989a30b6bb7f87193c2
Makhonin <maxon.07@mail.ru>
2024-11-28 11:53:41 +0300

    Добавлена возможность создавать треды

--- src/main/java/com/project/database_2b2t_org_ru/controller/MainController.java
+++ src/main/java/com/project/database_2b2t_org_ru/controller/MainController.java
@@ -5,8 +5,11 @@ import com.project.database_2b2t_org_ru.service.interfaces.ThreadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -18,7 +21,25 @@ public class MainController {
     @RequestMapping("/")
     public String showIndex(Model model) {
         List<Thread> threads = threadService.getAllObjects();
+        Thread thread = new Thread();
         model.addAttribute("threads", threads);
+        model.addAttribute("newThread", thread);
         return "index";
     }
+
+    @RequestMapping("/{id}")
+    public String showThread(@PathVariable("id") int id, Model model){
+        Thread thread = threadService.getObjectById(id);
+        model.addAttribute("thread", thread);
+        return "thread";
+    }
+
+    @RequestMapping("/makeThread")
+    public String makeThread(@ModelAttribute("newThread") Thread thread, Model model){
+        thread.setDateTime(LocalDateTime.now());
+        threadService.saveObject(thread);
+        model.addAttribute( "thread", thread);
+        return showThread(thread.getId(), model);
+    }
+
 }
